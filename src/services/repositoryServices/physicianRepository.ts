db006d89ae6b0e7f682d07c8ccbda1b69ae2e110
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-11-11 22:44:22 +0530

    feat: implement createPhysician method in PhysicianRepository to execute spCreatePhysician stored procedure with all physician parameters

--- src/services/repositoryServices/physicianRepository.ts
+++ src/services/repositoryServices/physicianRepository.ts
@@ -1,5 +1,6 @@
 import PhysicianMapper from "../../helpers/mapper/physicianMapper.js";
 import { getMSSQLConnection } from "../../helpers/mssql.js";
+import type { Physician } from "../../models/physician.js";
 
 class PhysicianRepository {
 
@@ -20,7 +21,6 @@ class PhysicianRepository {
             try {
                 const pool = await getMSSQLConnection();
                 const request = pool.request();
-                // Add your SQL query and parameters here
     
                 const result = await request.query( `exec [physician].[spGetphysicianByGuid] @physician_guid='${physicianGuid}'`);
     
@@ -31,20 +31,51 @@ class PhysicianRepository {
             }
         }   
 
-            async getAllphysicians(pageNumber: Number, pageSize: Number): Promise<any> {
-                try {
-                    const pool = await getMSSQLConnection();
-                    const request = pool.request();
-                    // Add your SQL query and parameters here
+        async getAllphysicians(pageNumber: Number, pageSize: Number): Promise<any> {
+            try {
+                const pool = await getMSSQLConnection();
+                const request = pool.request();
         
-                    const result = await request.query( `exec [physician].[spGetAllphysicians] @pageNumber = '${pageNumber}',@pageSize = '${pageSize}'`);
+                 const result = await request.query( `exec [physician].[spGetAllphysicians] @pageNumber = '${pageNumber}',@pageSize = '${pageSize}'`);
         
-                    return result.recordset.map((record: any)=> this.physicianMapper.mapToPhysician(record));
+                return result.recordset.map((record: any)=> this.physicianMapper.mapToPhysician(record));
         
-                } catch (error) {
-                    return Promise.reject(error);
-                }
+            } catch (error) {
+                return Promise.reject(error);
+            }
             
+        }
+
+        async createPhysician (physician: Physician): Promise<any> 
+        {
+            try
+            {
+                const pool = await getMSSQLConnection();
+                const request = pool.request();
+
+                const result = await request.query
+                (`
+                    exec
+                    [physician].[spCreatePhysician] 
+                        @npi = '${physician.Npi}',
+                        @name = '${physician.Name}',
+                        @phone_number = '${physician.PhoneNumber}',
+                        @alternative_phone_number = '${physician.Npi}',
+                        @enumeration_date = '${physician.EnumerationDate}',
+                        @npiType = '${physician.NpiType}',
+                        @is_sole_proprietor = '${physician.IsSoleProprietor}',
+                        @is_active = '${physician.IsActive}',
+                        @mailing_address = '${physician.MailingAddress}',
+                        @primary_practice_address = '${physician.PrimaryPracticeAddress}',
+                        @secondary_practice_address = '${physician.SecondaryPracticeAddress}'`
+                );
+
+                return result.recordset;  
+
+             } catch (error) 
+             {
+                return Promise.reject(error);
+             }         
         }
 
 }
