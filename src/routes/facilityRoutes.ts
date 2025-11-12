caa6a603a6e45dda5059b20ac02c54fd8c4cc42e
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-11-12 22:56:10 +0530

    feat: add DELETE route for deleteFacilityByGuid in FacilityRoutes

--- src/routes/facilityRoutes.ts
+++ src/routes/facilityRoutes.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get('/getFacilityByGuid/:facility_guid', FacilityController.getFacilityByGuid);
 router.get('/getAllfacilities/:pageNumber/:pageSize', FacilityController.getAllfacilities);
 router.post('/createFacility', FacilityController.createFacility);
+router.delete('/deleteFacilityByGuid/:facilityGuid', FacilityController.deleteFacilityByGuid);
 
 export default router;
