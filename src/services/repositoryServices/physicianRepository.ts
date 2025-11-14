b48a13e46ec4a40c434640be1ed3d894d16cc26e
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-11-14 23:43:03 +0530

    docs: add meaningful JSDoc comments and improve formatting in PhysicianRepository

--- src/services/repositoryServices/physicianRepository.ts
+++ src/services/repositoryServices/physicianRepository.ts
@@ -6,61 +6,71 @@ class PhysicianRepository {
 
     private physicianMapper: PhysicianMapper;
 
-    constructor(){
+    constructor() {
         this.physicianMapper = new PhysicianMapper();
     }
 
-    // Implementation of PatientsRepository methods
     /**
-         * 
-         * @param physicianGuid 
-         * @returns 
-         */
-    
-        async getPhysicianByGuid(physicianGuid: string): Promise<any> {
-            try {
-                const pool = await getMSSQLConnection();
-                const request = pool.request();
-    
-                const result = await request.query( `exec [physician].[spGetphysicianByGuid] @physician_guid='${physicianGuid}'`);
-    
-                return result.recordset;
-    
-            } catch (error) {
-                return Promise.reject(error);
-            }
-        }   
+     * Fetches a physician record from the database using its GUID.
+     * 
+     * @param physicianGuid - Unique identifier of the physician.
+     * @returns Promise resolving with physician details.
+     */
+    async getPhysicianByGuid(physicianGuid: string): Promise<any> {
+        try {
+            const pool = await getMSSQLConnection();
+            const request = pool.request();
 
-        async getAllphysicians(pageNumber: Number, pageSize: Number): Promise<any> {
-            try {
-                const pool = await getMSSQLConnection();
-                const request = pool.request();
-        
-                 const result = await request.query( `exec [physician].[spGetAllphysicians] @pageNumber = '${pageNumber}',@pageSize = '${pageSize}'`);
-        
-                return result.recordset.map((record: any)=> this.physicianMapper.mapToPhysician(record));
-        
-            } catch (error) {
-                return Promise.reject(error);
-            }
-            
+            const result = await request.query(`exec [physician].[spGetphysicianByGuid] @physician_guid='${physicianGuid}'`);
+
+            return result.recordset;
+
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
+
+    /**
+    * Retrieves all physicians with pagination.
+    * 
+    * @param pageNumber - Current page.
+    * @param pageSize - Number of records per page.
+    * @returns Promise resolving with mapped physicians list.
+    */
+    async getAllphysicians(pageNumber: Number, pageSize: Number): Promise<any> {
+        try {
+            const pool = await getMSSQLConnection();
+            const request = pool.request();
+
+            const result = await request.query(`exec [physician].[spGetAllphysicians] @pageNumber = '${pageNumber}',@pageSize = '${pageSize}'`);
+
+            return result.recordset.map((record: any) => this.physicianMapper.mapToPhysician(record));
+
+        } catch (error) {
+            return Promise.reject(error);
         }
 
-        async createPhysician (physician: Physician): Promise<any> 
-        {
-            try
-            {
-                const pool = await getMSSQLConnection();
-                const request = pool.request();
+    }
 
-                const result = await request.query
+    /**
+     * Creates a new physician record in the database.
+     * 
+     * @param physician - Physician object with required fields.
+     * @returns Promise resolving with created physician record.
+     */
+    async createPhysician(physician: Physician): Promise<any> {
+        try {
+            const pool = await getMSSQLConnection();
+            const request = pool.request();
+
+            const result = await request.query
                 (`
                     exec
                     [physician].[spCreatePhysician] 
                         @npi = '${physician.Npi}',
                         @name = '${physician.Name}',
                         @phone_number = '${physician.PhoneNumber}',
-                        @alternative_phone_number = '${physician.Npi}',
+                        @alternative_phone_number = '${physician.AlternativePhoneNumber}',
                         @enumeration_date = '${physician.EnumerationDate}',
                         @npiType = '${physician.NpiType}',
                         @is_sole_proprietor = '${physician.IsSoleProprietor}',
@@ -70,26 +80,31 @@ class PhysicianRepository {
                         @secondary_practice_address = '${physician.SecondaryPracticeAddress}'`
                 );
 
-                return result.recordset;  
+            return result.recordset;
 
-             } catch (error) 
-             {
-                return Promise.reject(error);
-             }         
+        } catch (error) {
+            return Promise.reject(error);
         }
+    }
 
-        async deletePhysicianByGuid(physicianGuid: string): Promise<any> {
-            try {
-                const pool = await getMSSQLConnection();
-                const request = pool.request();
-                
-                const result = await request.query( `exec [physician].[spDeletePhysicianByGuid] @physician_guid='${physicianGuid}'`);
-    
-                return result.recordset;
-            } catch (error) {
-                return Promise.reject(error);
-            }
+    /**
+     * Deletes a physician record based on GUID.
+     * 
+     * @param physicianGuid - GUID of the physician to delete.
+     * @returns Promise resolving when deletion is successful.
+     */
+    async deletePhysicianByGuid(physicianGuid: string): Promise<any> {
+        try {
+            const pool = await getMSSQLConnection();
+            const request = pool.request();
+
+            const result = await request.query(`exec [physician].[spDeletePhysicianByGuid] @physician_guid='${physicianGuid}'`);
+
+            return result.recordset;
+        } catch (error) {
+            return Promise.reject(error);
         }
+    }
 
 }
 
