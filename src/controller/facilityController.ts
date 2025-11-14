ba7fbcc944d9be971392835de5406cb1221f5917
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-11-14 23:54:02 +0530

    docs: add JSDoc comments to FacilityController methods

--- src/controller/facilityController.ts
+++ src/controller/facilityController.ts
@@ -1,9 +1,14 @@
-import express, {type Request, type Response } from 'express';
+import express, { type Request, type Response } from 'express';
 import FacilityBusinessService from "../services/bussinessServices/facilityBusinessService.js";
 
 class FacilityController {
 
-    async getFacilityByGuid (req: Request, res: Response): Promise<any> {   
+    /**
+     * Fetch a facility by its GUID
+     * @param req Express request object
+     * @param res Express response object
+     */
+    async getFacilityByGuid(req: Request, res: Response): Promise<any> {
         const facilityBusinessService = new FacilityBusinessService();
         const { facility_guid } = req.params;
         if (!facility_guid) {
@@ -11,16 +16,26 @@ class FacilityController {
         }
         const result = await facilityBusinessService.getFacilityByGuid(facility_guid);
         return res.json({ result });
-    } 
+    }
 
-    async getAllfacilities (req: Request, res: Response): Promise<any> {   
+    /**
+     * Fetch paginated list of all facilities
+     * @param req Express request object
+     * @param res Express response object
+     */
+    async getAllfacilities(req: Request, res: Response): Promise<any> {
         const facilityBusinessService = new FacilityBusinessService();
         const pageNumber = parseInt(req.query.pageNumber as string) || 1;
-        const pageSize = parseInt(req.query.pageSize as string) || 10; 
+        const pageSize = parseInt(req.query.pageSize as string) || 10;
         const result = await facilityBusinessService.getAllfacilities(pageNumber, pageSize);
         return res.json({ result });
     }
 
+    /**
+     * Create a new facility
+     * @param req Express request containing facility data
+     * @param res Express response object
+     */
     async createFacility(req: Request, res: Response): Promise<any> {
         const facilityBusinessService = new FacilityBusinessService();
         const facility = req.body;
@@ -28,6 +43,11 @@ class FacilityController {
         return res.json({ result });
     }
 
+    /**
+     * Delete a facility by its GUID
+     * @param req Express request object
+     * @param res Express response object
+     */
     async deleteFacilityByGuid(req: Request, res: Response): Promise<any> {
         const facilityBusinessService = new FacilityBusinessService();
         const { facilityGuid } = req.params;
